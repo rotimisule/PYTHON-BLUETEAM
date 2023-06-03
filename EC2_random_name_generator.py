98fc9299136e7f95b366040f6f469f85dc060a4f
rotimisule <rotimisule24@gmail.com>
2023-06-03 01:40:17 +0000

    Ec2 random name generator

--- EC2_random_name_generator.py
+++ EC2_random_name_generator.py
@@ -7,11 +7,15 @@ department = input("What deperatment are you creating for today ? ")
 department1 = "Marketing"
 department2 = "Accounting"
 department3 = "FinOps"
+##function
 ##check if the user is in one of the 3 departments
 def name_generator_func(ec2_num, department):
     if department.casefold() in (department1.casefold(), department2.casefold(), department3.casefold()) :
+##use Instance number to give loop a range for EC2 random name generator
         for x in range(ec2_num):
+##create random letters + numbers along with a number of chars defined as k 
             randomchars = ''.join(random.choices(string.digits + string.ascii_letters , k=8))
+#print the department entered with a captial letter + a undrscore and the random letters generated 
             print(department.capitalize() +'_'+randomchars)
     else :
         print ("You do not have permission to use this name generator, goodbye.")
