c848fe58be0fc892b68c0fdc1f9d759a4560b55d
rotimisule <rotimisule24@gmail.com>
2023-06-07 19:28:16 +0000

    Hackerrank print square in a range

--- Hackerank_print_square.py
+++ Hackerank_print_square.py
@@ -1,9 +1,12 @@
-n=int(input())
-if n<21 and n >0:
-    for i in range(-1, n-1):
-        i = i +1
-        new= i * i
-        print (new)
+n = int(input())  # Prompt the user to enter an integer value for n.
+
+
+if n < 21 and n > 0:  # Check if n is between 1 and 20 (inclusive).
+    for i in range(-1, n-1):  # Iterate over the range from -1 to n-1.
+        i = i + 1  # Increment i by 1.
+        new = i * i  # Calculate the square of i and name it new.
+        print(new)  # Print the value of new (the square of i).
+
         
 
     
