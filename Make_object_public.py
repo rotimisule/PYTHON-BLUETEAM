4872b2e3be13807ba677ba2c1aa92a532b08bee8
rotimisule <rotimisule24@gmail.com>
2023-06-16 16:02:27 +0000

    Make bucket objects public

---
+++ Make_object_public.py
@@ -0,0 +1,39 @@
+import boto3 
+s3 = boto3.client('s3')
+bucket='rsule-boto3-june122023'
+key="Hello_glo_upload.txt"
+response = s3.put_public_access_block(
+    Bucket= bucket,
+    
+    
+    PublicAccessBlockConfiguration={
+        'BlockPublicAcls': False,
+        'IgnorePublicAcls': False,
+        'BlockPublicPolicy': False,
+        'RestrictPublicBuckets': False
+    }
+    
+)
+response = s3.put_bucket_ownership_controls(
+    Bucket=bucket,
+
+    OwnershipControls={
+        'Rules': [
+            {
+                'ObjectOwnership': 'BucketOwnerPreferred'
+            },
+        ]
+    }
+)
+response = s3.put_bucket_acl(
+    ACL='private',
+    
+    Bucket=bucket
+
+)
+response = s3.put_object_acl(ACL='public-read',
+                                    Bucket=bucket ,
+                                        Key=key)
+                                        
+                                        
+print(response)
