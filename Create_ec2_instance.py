a41a6b56eb1c60a82e998a111ea96698533b5e50
rotimisule <rotimisule24@gmail.com>
2023-06-21 02:37:13 +0000

    Creating an ec2 instance with Userdata

--- Create_ec2_instance.py
+++ Create_ec2_instance.py
@@ -1,20 +1,39 @@
 import boto3
+
+
 ec2=boto3.client('ec2')
-ami="ami-0996a91449839b5c2"
+ami_id="ami-024e6efaf93d85776"
 key="GLO-Pair"
 sg="sg-0b285d2deaab69a7e"
 
-response = ec2.run_instances(
-  
-    ImageId=ami,
-    InstanceType='t2.micro',
-    KeyName=key,
-    MaxCount=1,
-    MinCount=1,
-    SecurityGroupIds=[
-        sg,
-    ],
-  
-)
+user_data= '''#!/bin/bash
+    apt update-y
+    apt -get install -y apache2
+    systemctl start apache
+    systemctl enable apache2'''
 
-print(response["Instances"][0]["InstanceId"])
+
+
+def create_instance(client , ami_id, sg, key, user_data=None):
+
+    response = client.run_instances(
+      
+        ImageId=ami_id,
+        InstanceType='t2.micro',
+        KeyName=key,
+        MaxCount=1,
+        MinCount=1,
+        SecurityGroupIds=[
+            sg,
+        ],
+        UserData=user_data
+      
+    )
+    print(response["Instances"][0]["InstanceId"])
+    
+create_instance(ec2, ami_id, sg,key, user_data)
+
+    
+
+
+    
